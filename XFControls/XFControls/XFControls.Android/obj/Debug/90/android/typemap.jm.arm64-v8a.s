04d754e01bf5101a1223faa873529da2e634d6a7
Carlos Andrew Costa Bezerra <andrew@r2asistemas.com.br>
2019-10-23 16:44:24 -0300

    Adicionando arquivos do projeto

---
+++ XFControls/XFControls/XFControls.Android/obj/Debug/90/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 169596760d8296a63e6121cf4f5c549011ffb519 */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1282
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 335885
+	.include	"typemap.jm.inc"
